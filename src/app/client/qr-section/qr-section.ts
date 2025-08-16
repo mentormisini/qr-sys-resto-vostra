04d3caaefa01300ccdf738575dbeb342c5188d2a
MISINI Mentor <misinimentor@gmail.com>
2025-08-16 21:23:14 +0200

    confeti and qr

--- src/app/client/qr-section/qr-section.ts
+++ src/app/client/qr-section/qr-section.ts
@@ -1,6 +1,9 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {QRCodeComponent} from 'angularx-qrcode';
 import html2canvas from 'html2canvas';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-qr-section',
@@ -12,20 +15,46 @@ import html2canvas from 'html2canvas';
 })
 export class QrSection {
   qrData = 'DISCOUNT-20%-ONCE-ONLY';
-  @ViewChild('card', { static: false }) card!: ElementRef;
+  @ViewChild('qrContainer', { static: false }) qrContainer!: ElementRef;
+  http= inject(HttpClient)
+  QrData:any;
+  getQR():Observable<any>{
+    return this.http.post(`http://localhost:3000/api/discount-qrcode`,{})
+  }
 
-  saveScreenshot() {
-    if (!this.card) return;
+  ngOnInit(){
+    this.getQR().subscribe(resp=>{
+      this.QrData = resp;
+      this.triggerConfetti()
+    })
+  }
 
-    html2canvas(this.card.nativeElement).then(canvas => {
-      canvas.toBlob(blob => {
-        if (!blob) return;
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = 'discount-card.png';
-        link.click();
-        URL.revokeObjectURL(link.href);
+
+  triggerConfetti() {
+    var end = Date.now() + (1000);
+
+
+    const colors = ['#1e9304', '#e00909','#999'];
+
+    (function frame() {
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
       });
-    });
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+      });
+
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
   }
 }
